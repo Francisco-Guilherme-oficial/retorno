216eb4240eb3dee3284dc9842c421e27b228869f
Francisco Guilherme <frcog.monteiro@gmail.com>
2024-07-05 08:26:59 -0300

    adicionando outro comit

--- exemplo.py
+++ exemplo.py
@@ -1,5 +1,3 @@
 print("Olá, Boas vindas ao VSCode!"); nome = input("Digite seu nome para fazer o login: ");senha = int(input("Digite a senha: "))
-if nome == "Francisco" and senha == 123:
-    print("Olá!", nome, "boas vindas ao VSCode!")
-else:
-    print("Senha ou Usuario incorreto")
+if nome == "Francisco" and senha == 123: print("Olá!", nome, "boas vindas ao VSCode!")
+else: print("Senha ou Usuario incorreto")
